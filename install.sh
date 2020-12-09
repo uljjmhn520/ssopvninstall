e86ce83e8ca0313aaaa6081404eecc19f63caf99
uljjmhn520 <284141050@qq.com>
2020-12-09 15:19:07 +0800

    alter a little

--- install.sh
+++ install.sh
@@ -57,9 +57,11 @@ echo "#####################################"
 echo "######## install docker-compose ############"
 echo "#####################################"
 
-sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-
-sudo chmod +x /usr/local/bin/docker-compose
+docker_compose_path=/usr/local/bin/docker-compose
+if [ ! -x $docker_compose_path ]; then
+sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o $docker_compose_path
+sudo chmod +x $docker_compose_path
+fi
 
 echo "#####################################"
 echo "######## docker ss and openvpn ############"
